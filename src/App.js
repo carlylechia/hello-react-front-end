9a8a1ce21fa95836abbc0f37222fe7dd22dc78fc
Chia Carlyle <chiacarlyle@gmail.com>
2022-09-13 22:17:30 +0100

    Use react to render data from the rails api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import Messages from './components/messages';
+
+const API_URL = 'http://localhost:3000/api/v1/messages';
+
+function getApiData() {
+  return axios.get(API_URL).then(response => response.data)
+}
 
 function App() {
+  const [message, setMessage] = useState([]);
+
+  useEffect(() => {
+    let mounted = true;
+    getApiData().then((item) => {
+      if (mounted) {
+        setMessage(item.text)
+        console.log(item.text)
+      }
+    });
+    return () => (mounted = false);
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Messages message={message} />
     </div>
   );
 }
---
+++ src/components/messages.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Messages(props) {
+  return (
+    <h1>{props.message}</h1>
+  )
+}
+
+export default Messages
