eedf319e8054823d3363b1dcf40be615b45902a2
Chia Carlyle <chiacarlyle@gmail.com>
2022-09-14 11:43:31 +0100

    Add redux

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
 import './App.css';
-import axios from 'axios';
-import { useState, useEffect } from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { store } from './Redux/store';
 import Messages from './components/messages';
 
-const API_URL = 'http://localhost:3000/api/v1/messages';
-
-function getApiData() {
-  return axios.get(API_URL).then(response => response.data)
-}
-
 function App() {
-  const [message, setMessage] = useState([]);
-
-  useEffect(() => {
-    let mounted = true;
-    getApiData().then((item) => {
-      if (mounted) {
-        setMessage(item.text)
-        console.log(item.text)
-      }
-    });
-    return () => (mounted = false);
-  }, []);
   return (
-    <div className="App">
-      <Messages message={message} />
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Messages />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- src/components/messages.js
+++ src/components/messages.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMessage } from '../Redux/store';
 
-function Messages(props) {
+function Messages() {
+  useEffect(() => {
+    dispatch(fetchMessage());
+  }, []);
+
+  const dispatch = useDispatch();
+  const data = useSelector(state => state).text
+
   return (
-    <h1>{props.message}</h1>
+    <div className='App'>
+      <h1>{data}</h1>
+    </div>
   )
 }
 
